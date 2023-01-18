4b95b7e247cea3181d8ad4520160784d18b3407c
meet-bariya <marutimeet@gmail.com>
2023-01-18 22:53:57 +0530

    allowed host list updated

--- recipe/settings.py
+++ recipe/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['*.railway.app','localhost','127.0.0.1','0.0.0.0']
+ALLOWED_HOSTS = [config('ALLOWED_HOSTS').split(',')]
 
 
 # Application definition
