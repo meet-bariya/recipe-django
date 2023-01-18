b7d2c69ba5d6a59df869776d71310982eab035b7
meet-bariya <marutimeet@gmail.com>
2023-01-18 22:47:25 +0530

    host info updated

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,6 @@ RUN pip3 install --no-cache -r requirements.txt
 
 COPY . . 
 
-CMD gunicorn recipe.wsgi:application --bind 0.0.0.0:8000
+CMD gunicorn recipe.wsgi:application --bind 0.0.0.0:80
 
-EXPOSE 8000 
+EXPOSE 80
--- recipe/settings.py
+++ recipe/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['*.railway.app','0.0.0.0']
+ALLOWED_HOSTS = ['*.railway.app','localhost','127.0.0.1','0.0.0.0']
 
 
 # Application definition
