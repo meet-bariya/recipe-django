84643cf9bbb8dce10cf7a4a031e2011040d78c92
meet-bariya <marutimeet@gmail.com>
2023-01-20 17:07:15 +0530

    template formating and prod settings added

--- recipe/settings.py
+++ recipe/settings.py
@@ -31,6 +31,7 @@ DEBUG = config('DEBUG')
 # ALLOWED_HOSTS = [config('ALLOWED_HOSTS').split(',')]
 ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
+CSRF_TRUSTED_ORIGINS = ["https://" + x for x in config('ALLOWED_HOSTS').split(',')]
 # Application definition
 
 INSTALLED_APPS = [
@@ -54,6 +55,7 @@ MESSAGE_TAGS = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+        'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -134,6 +136,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
